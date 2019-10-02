8651a8fc1648ece880a826a9b7f1ee8bd2717a18
Valentin Evteev <stratumforce@protonmail.com>
2019-10-02 19:03:55 +0300

    fix: change matchAll method to match for support on node 10

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 module.exports = function zeros(expression) {
   // Get all factorials
-  const factorials = [...expression.matchAll(/([\d\!]+)/g)];
+  const factorials = expression.match(/([\d\!]+)/g);
 
   let twos = 0;
   let fives = 0;
 
   factorials.forEach(factorial => {
     // Get the number
-    let num = factorial[0].match(/([\d]+)/g)[0];
+    let num = factorial.match(/([\d]+)/g)[0];
     // Get the step: 1 for regular factorial and 2 for double factorial
-    let step = factorial[0].includes("!!") ? 2 : 1;
+    let step = factorial.includes("!!") ? 2 : 1;
 
     // Count the twos and fives
     while (num >= 2) {
